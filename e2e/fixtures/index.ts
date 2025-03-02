3ab6e44f9f7a5d98bf5b498a6895c11a2895dcaf
Prajna0106 <prajnajeet02@gmail.com>
2025-03-02 18:55:34 +0000

    Added Task 1

---
+++ e2e/fixtures/index.ts
@@ -0,0 +1,20 @@
+import { test as base } from "@playwright/test";
+import LoginPage from "../poms/login";
+import Task1Page from "../poms/task1";
+
+interface ExtendedFixtures {
+  loginPage: LoginPage;
+  task1Page: Task1Page;
+}
+
+export const test = base.extend<ExtendedFixtures>({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await use(loginPage);
+  },
+
+  task1Page: async ({ page }, use) => {
+    const task1Page = new Task1Page(page);
+    await use(task1Page);
+  },
+});
